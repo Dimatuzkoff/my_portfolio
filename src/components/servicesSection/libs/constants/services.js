bf36a43de7bdf18695012ade85141e84ff428711
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-05-22 21:45:40 +0300

    added skills

---
+++ src/components/servicesSection/libs/constants/services.js
@@ -0,0 +1,37 @@
+export const services = [
+  {
+    id: 1,
+    icon: "/vue.svg",
+    name: "Vue.js",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
+  },
+  {
+    id: 2,
+    icon: "/nuxt.svg",
+    name: "Nuxt.js",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
+  },
+  {
+    id: 3,
+    icon: "/react.svg",
+    name: "React.js",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
+  },
+  {
+    id: 4,
+    icon: "/node.svg",
+    name: "Node.js",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
+  },
+  {
+    id: 5,
+    icon: "/git.svg",
+    name: "Git",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
+  },
+];
--- src/components/servicesSection/ui/ServicesSection.vue
+++ src/components/servicesSection/ui/ServicesSection.vue
@@ -2,62 +2,20 @@
 import { ref } from "vue";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { services } from "../libs/constants/services";
 AOS.init();
-const services = ref([
-  {
-    id: 1,
-    icon: "https://img.icons8.com/ios-filled/100/ffffff/google-code.png",
-    name: "WEB DEVELOPMENT",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
-  },
-  {
-    id: 2,
-    icon: "https://img.icons8.com/external-vectorslab-glyph-vectorslab/100/fffeee/external-Technical-Writing-mobile-app-development-vectorslab-glyph-vectorslab.png",
-    name: "Technical Writing",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
-  },
-  {
-    id: 3,
-    icon: "https://img.icons8.com/external-vectorslab-glyph-vectorslab/100/fffeee/external-Technical-Writing-mobile-app-development-vectorslab-glyph-vectorslab.png",
-    name: "Mobile Development",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
-  },
-  {
-    id: 4,
-    icon: "https://img.icons8.com/ios-filled/100/ffffff/email-open.png",
-    name: "WEB DEVELOPMENT",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
-  },
-  {
-    id: 5,
-    icon: "https://img.icons8.com/ios-filled/100/ffffff/windows10-personalization.png",
-    name: "Graphic Design",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
-  },
-  {
-    id: 6,
-    icon: "https://img.icons8.com/ios-filled/100/ffffff/web-design.png",
-    name: "Web Design",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, ut!",
-  },
-]);
+const myServices = ref(services);
 </script>
 <template>
   <section class="text-white mt-20" id="services">
     <div class="px-4 xl:pl-16" data-aos="flip-right">
-      <h2 class="text-4xl font-bold text-white mb-4">My services</h2>
+      <h2 class="text-4xl font-bold text-white mb-4">Мій стек технологій</h2>
     </div>
     <div
       class="py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3"
     >
       <div
-        v-for="service in services"
+        v-for="service in myServices"
         :key="service.id"
         data-aos="fade-up"
         class="px-8 py-12 rounded-xl bg-[#111a3e] shadow-lg border border-[#1f1641]"
@@ -69,13 +27,13 @@ const services = ref([
         </div>
         <div class="text-center">
           <h3
-            class="pt-8 text-lg font-semibold uppercase text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary lg:text-xl"
+            class="pt-8 text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary lg:text-xl"
           >
             {{ service.name }}
           </h3>
-          <p class="text-grey pt-4 text-sm group-hover:text-white md:text-base">
+          <!-- <p class="text-grey pt-4 text-sm group-hover:text-white md:text-base">
             {{ service.description }}
-          </p>
+          </p> -->
         </div>
       </div>
     </div>
