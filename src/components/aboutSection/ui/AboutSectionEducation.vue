49d5c410a3690967bf38db3862448427f0d670fa
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-05-29 23:33:57 +0300

    fix

--- src/components/aboutSection/ui/AboutSectionEducation.vue
+++ src/components/aboutSection/ui/AboutSectionEducation.vue
@@ -5,6 +5,7 @@ import AboutSectionEducationItem from "./AboutSectionEducationItem.vue";
 import AboutSectionEducationMe from "./AboutSectionEducationMe.vue";
 const myEducation = ref(education);
 </script>
+
 <template>
   <div
     class="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 z-1"
