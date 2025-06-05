e1c39eb8ec74bf6a79ddc0bd82f39a0aa372408b
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-05 19:35:32 +0300

    created project section

--- src/components/projectSection/ui/ProjectSection.vue
+++ src/components/projectSection/ui/ProjectSection.vue
@@ -1,5 +1,12 @@
 <script setup>
 </script>
 <template>
-  <div class=""></div>
+  <section class="text-white mt-20">
+    <div class="px-4 xl:pl-16">
+      <div class="mb-4 md:flex md:justify-between xl:pr-16">
+        <h2 class="text-4xl font-bold text-white">Мої проекти</h2>
+        <div class=""></div>
+      </div>
+    </div>
+  </section>
 </template>
