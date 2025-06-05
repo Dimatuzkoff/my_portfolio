c5c21d6e36cd10444187170d13054a2fea1fb18e
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-06-05 19:50:08 +0300

    added category of project

---
+++ src/components/projectSection/libs/constants/categoriesItems.js
@@ -0,0 +1,19 @@
+export const categoriesItems = [
+  {
+    id: 1,
+    name: "All",
+  },
+  {
+    id: 2,
+    name: "Vue/Nuxt",
+  },
+  {
+    id: 3,
+    name: "React",
+  },
+
+  {
+    id: 4,
+    name: "Html/Css",
+  },
+];
--- src/components/projectSection/ui/ProjectSection.vue
+++ src/components/projectSection/ui/ProjectSection.vue
@@ -1,11 +1,21 @@
 <script setup>
+import { ref } from "vue";
+import { categoriesItems } from "../libs/constants/categoriesItems";
 </script>
 <template>
   <section class="text-white mt-20">
     <div class="px-4 xl:pl-16">
       <div class="mb-4 md:flex md:justify-between xl:pr-16">
         <h2 class="text-4xl font-bold text-white">Мої проекти</h2>
-        <div class=""></div>
+        <div class="flex space-x-4 mb-4 mt-5 md:nt-0">
+          <button
+            class="hover:text-primary"
+            v-for="category in categoriesItems"
+            :key="category.id"
+          >
+            {{ category.name }}
+          </button>
+        </div>
       </div>
     </div>
   </section>
